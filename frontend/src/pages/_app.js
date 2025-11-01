efde723e53fc46e8912cbc8bd374524a5c9a4d30
winsznx <timjosh507@gmail.com>
2025-11-01 01:05:21 +0100

    feat: setup app providers with RainbowKit and React Query

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,22 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import '../styles/globals.css';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { config } from '../config/wagmi';
+import { Toaster } from 'react-hot-toast';
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+          <Toaster position="bottom-right" />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
